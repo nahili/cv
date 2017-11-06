495a0a964caf66b7ff983b220875be2fd93f6da8
Bastien Noverraz <bastien.noverraz@gmail.com>
2017-11-06 11:55:27 +0100

    Base Header component and HTML

--- src/app/cv.module.ts
+++ src/app/cv.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CVService } from './cv.service';
 
 import { CVComponent } from './cv.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule ({
   imports : [
     BrowserModule
   ],
   declarations : [
-    CVComponent
+    CVComponent,
+    HeaderComponent
   ],
   providers : [
     CVService
---
+++ src/app/header.component.ts
@@ -0,0 +1,19 @@
+import { Component,OnInit } from '@angular/core';
+
+import { CVService } from './cv.service';
+
+@Component({
+  selector:'cv-header',
+  templateUrl:'./header.component.html'
+})
+export class HeaderComponent implements OnInit {
+
+  header:any = null;
+
+  constructor(private cvService: CVService) { }
+
+  ngOnInit():void {
+    this.header = this.cvService.getCV().header;
+  }
+
+}
