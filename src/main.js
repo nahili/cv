e2bdca0d3e213175d940db39f6a4a6919b722b2a
Bastien Noverraz <bastien.noverraz@gmail.com>
2017-11-06 11:34:18 +0100

    Git: removed some generated files

--- src/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var cv_module_1 = require("./app/cv.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(cv_module_1.CVModule);
-//# sourceMappingURL=main.js.map
