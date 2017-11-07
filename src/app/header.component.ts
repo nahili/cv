3095d7ca22e28c5bdb58d9e7c514fb777df67570
Bastien Noverraz <bastien.noverraz@gmail.com>
2017-11-07 13:00:36 +0100

    Very basic header design

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,8 +3,9 @@ import { Component,OnInit } from '@angular/core';
 import { CVService } from './cv.service';
 
 @Component({
-  selector:'cv-header',
-  templateUrl:'./header.component.html'
+  selector: 'cv-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
