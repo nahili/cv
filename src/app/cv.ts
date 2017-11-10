d7d68b14bead13758f212c566d98db69e112204b
Bastien Noverraz <bastien.noverraz@gmail.com>
2017-11-10 18:19:34 +0100

    Some corrections on the data

--- src/app/cv.ts
+++ src/app/cv.ts
@@ -5,10 +5,10 @@ export const CV: any = {
     firstname:"Bastien",
     age:"28",
     diploma:"Bachelor in Computer Science",
-    experience:"5 ans",
+    experience:"4 ans",
     disponibility: {
-      remote:"right now",
-      local:"12 décembre"
+      remote:"maintenant",
+      local:"le 12 décembre"
     },
   },
   abilities: [
@@ -79,7 +79,7 @@ export const CV: any = {
       from:"2014",
       to:"2016",
       title:"Transports publics de la région lausannoise SA / Développeur",
-      description:"Travail au sein de l'équipe informatique pour la partie information voyageur. Logiciel open-source spécialisé dans la gestion d'horaire en C++/SQL pour le backend, et travail avec des intervenants externes pour le développement du site web, des paneaux d'affichages aux arrêts, dans les bus et sur l'application mobile. Au cours de ce mandat la plateforme embarquée dans les bus a été refaite afin d'être plus maintenable et flexible.",
+      description:"Travail au sein de l'équipe informatique pour la partie information voyageur. Logiciel open-source spécialisé dans la gestion d'horaire en C++/SQL pour le backend et travail avec des intervenants externes pour le développement du site web, des paneaux d'affichage aux arrêts, dans les bus et sur l'application mobile. Au cours de ce mandat la plateforme embarquée dans les bus a été refaite afin d'être plus maintenable et flexible.",
       abilities:["Gestion de projet","C++","Angular","Web","CouchDB","OpenSuse","Docker","GIT","Travail d'équipe","Zabbix"],
       contact:{
         name:"BEAUSSAULT Stéphane",
@@ -93,33 +93,44 @@ export const CV: any = {
       to:"2014",
       title:"Policlinique Médicale Universitaire / Civiliste",
       description:"Travail varié, création et administration d'un système de serveurs Web virtuels pour les besoins du service informatique. Travail sur le CMS Allegro pour les RH. Création d'un site web mobile en Drupal.",
-      abilities:["Proxmox","Apache","Allegro","Zabbix","Observium","Drupal","Joomla","Web","MySQL","Debian"]
+      abilities:["Proxmox","Apache","Allegro","Zabbix","Observium","Drupal","Joomla","Web","MySQL","Debian"],
+      contact:{
+        name:"THABARD Julien",
+        email:"julien.thabard@hospvd.ch",
+        phone:"+41213146093"
+      }
     },
     {
       duration:"7 mois",
       from:"2012",
       to:"2013",
       title:"Compassion Suisse / Civiliste",
-      description:"Dans une petite équipe qu'est la branche suisse de Compassion International, le travail consiste aussi bien au développement en Windev de l'application principale utilisée dans l'entreprise que la mise en place et gestion de serveurs physiques et virtuels.",
-      abilities:["Windows 7","Windows Server 2008","Windev","ESXi","Debian","Archlinux"]
+      description:"Dans une petite équipe qu'est la branche Suisse de Compassion International. Le travail consiste aussi bien au développement en Windev de l'application principale utilisée dans l'entreprise que la mise en place et gestion de serveurs physiques et virtuels.",
+      abilities:["Windows 7","Windows Server 2008","Windev","ESXi","Debian","Archlinux"],
+      contact: {
+        name:"WULLIAMOZ David",
+        email:"dwulliamoz@compassion.ch",
+        phone:"+41244342126"
+      }
     },
     {
       duration:"1.5 ans",
       from:"2011",
       to:"2013",
       title:"HEIG-VD / Ingénieur assistant",
-      description:"Création d'un logiciel médicale en Qt/C++, de l'écriture de la spécification avec les membres du corps médical impliqué jusqu'à une implémentation en version alpha utilisable. Ce projet continue sous le nom d'Ezechiel.",
-      abilities:["Gestion de projet","C++","Qt","XML","SQL"]
+      description:"Création d'un logiciel médical en Qt/C++, de l'écriture de la spécification avec les membres du corps médical impliqués jusqu'à une implémentation en version alpha utilisable. Ce projet continue sous le nom d'Ezechiel.",
+      abilities:["Gestion de projet","C++","Qt","XML","SQL"],
+      contact:{
+        name:"THOMA Yann",
+        email:"yann.thoma@heig-vd.ch",
+        phone:"+41245576273"
+      }
     }
   ],
   hobbies:[
     {
       title:"Sports",
       elements:["Escalade","Cyclisme","Taekwondo","Badminton"]
-    },
-    {
-      title:"Projets",
-      elements:["Permapp","Blog de voyage"]
     }
   ],
   footer:{
@@ -130,12 +141,8 @@ export const CV: any = {
         url:"https://github.com/nahili/cv"
       },
       {
-        title:"Version HTML",
-        url:"https://github.com/nahili/cv/master/cv.html"
-      },
-      {
         title:"Version PDF",
-        url:"https://github.com/nahili/cv/master/cv.pdf"
+        url:"https://raw.githubusercontent.com/nahili/cv/master/cv.pdf"
       }
     ]
   }
