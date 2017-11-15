d10b0db53b161fac399a5f7e5b807b18f892fd5b
Bastien Noverraz <bastien.noverraz@gmail.com>
2017-11-15 15:10:31 +0100

    Service : diploma added and experience and age calculated in the service

--- src/app/cv.service.ts
+++ src/app/cv.service.ts
@@ -15,7 +15,28 @@ export class CvService {
   private cv:any = CVS[0];
 
   person():Promise<any> {
-    return Promise.resolve(this.cv.person);
+    let p = this.cv.person;
+
+    // Calculate the age from the birthday
+    // From https://stackoverflow.com/questions/41792026/how-do-i-calculate-age-from-birth-date-in-angular-2-using-typescript
+    if (p.birthday) {
+      let timeDiff = Math.abs(Date.now() - p.birthday);
+      p.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+    }
+
+    // Calculate the experience from the sum of all experiences
+    let total = 0;
+    for (let e of this.cv.experiences)
+      if (!e.doesNotCount) // Ignore experience that does not count
+        total += Math.abs(e.from - e.to);
+    // This calcul is to have the experience by step of 0.5
+    p.experience = Math.ceil((total*2 / (1000 * 3600 * 24))/365)/2;
+
+    return Promise.resolve(p);
+  }
+
+  diploma():Promise<any> {
+    return Promise.resolve(this.cv.diploma);
   }
 
   abilites():Promise<Array<any>> {
